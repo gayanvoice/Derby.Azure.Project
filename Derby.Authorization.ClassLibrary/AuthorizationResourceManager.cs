05e154ceb27b4756b417c9fb5d69728244860b8b
gayanvoice <gayan.kuruppu@live.com>
2024-09-14 17:43:50 +0100

    Update: Derby.Authorization > delete, show, and get all exisitng role assignments

--- Derby.Authorization.ClassLibrary/AuthorizationResourceManager.cs
+++ Derby.Authorization.ClassLibrary/AuthorizationResourceManager.cs
@@ -4,10 +4,6 @@ using Azure.ResourceManager.Authorization;
 using Azure.Core;
 using Azure.ResourceManager.Authorization.Models;
 using Azure;
-using Azure.ResourceManager.Resources;
-using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
-using Microsoft.Graph;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace Derby.Authorization.ClassLibrary
 {
@@ -22,32 +18,50 @@ namespace Derby.Authorization.ClassLibrary
             _tenentId = tenentId;
             _principleId = principleId;
         }
-        public async Task<List<AuthorizationRoleDefinitionData>> GetListAuthorizationRoleDefinitionDataAsync(string resourceId)
+        public async Task<List<RoleAssignmentData>> GetRoleAssignmentDataListAsync(string resourceId)
         {
-            Console.WriteLine($"Getting Role Assignment Collection");
+            Console.WriteLine($"Getting Role Definition Data List");
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions() { TenantId = _tenentId });
+            ArmClient armClient = new ArmClient(defaultAzureCredential);
+            ResourceIdentifier resourceIdentifier = new ResourceIdentifier(resourceId);
+            RoleAssignmentCollection roleAssignmentCollection = armClient.GetRoleAssignments(resourceIdentifier);
+            List<RoleAssignmentData> roleAssignmentDataList = new List<RoleAssignmentData>();
+            await foreach (RoleAssignmentResource roleAssignmentResource in roleAssignmentCollection.GetAllAsync())
+            {
+                RoleAssignmentData roleAssignmentData = roleAssignmentResource.Data;
+                roleAssignmentDataList.Add(roleAssignmentData);
+                Console.WriteLine($"Id: {roleAssignmentData.Id} Scope: {roleAssignmentData.Scope} Display Name: {roleAssignmentData.Name}");
+            }
+            Console.WriteLine($"Count: {roleAssignmentDataList.Count()}");
+            return roleAssignmentDataList;
+        }
+        public async Task<List<AuthorizationRoleDefinitionData>> GetAuthorizationRoleDefinitionDataListAsync(string resourceId)
+        {
+            Console.WriteLine($"Getting Authorization Role Definition Data List");
             DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions() { TenantId = _tenentId });
             ArmClient armClient = new ArmClient(defaultAzureCredential);
             ResourceIdentifier resourceIdentifier = new ResourceIdentifier(resourceId);
             AuthorizationRoleDefinitionCollection authorizationRoleDefinitionCollection = armClient.GetAuthorizationRoleDefinitions(resourceIdentifier);
             List<AuthorizationRoleDefinitionData> authorizationRoleDefinitionDataList = new List<AuthorizationRoleDefinitionData>();
-            await foreach (AuthorizationRoleDefinitionResource item in authorizationRoleDefinitionCollection.GetAllAsync())
+            await foreach (AuthorizationRoleDefinitionResource authorizationRoleDefinitionResource in authorizationRoleDefinitionCollection.GetAllAsync())
             {
-                authorizationRoleDefinitionDataList.Add(item.Data);
+                AuthorizationRoleDefinitionData authorizationRoleDefinitionData = authorizationRoleDefinitionResource.Data;
+                authorizationRoleDefinitionDataList.Add(authorizationRoleDefinitionData);
             }
             Console.WriteLine($"Count: {authorizationRoleDefinitionDataList.Count()}");
             return authorizationRoleDefinitionDataList;
         }
-        private async Task<AuthorizationRoleDefinitionData> GetAuthorizationRoleDefinitionData(string resourceId, string name)
+        public async Task<AuthorizationRoleDefinitionData> GetAuthorizationRoleDefinitionData(string resourceId, string name)
         {
             Console.WriteLine($"Getting Authorization Role Definition Data");
-            List<AuthorizationRoleDefinitionData> authorizationRoleDefinitionDataList = await GetListAuthorizationRoleDefinitionDataAsync(resourceId);
+            List<AuthorizationRoleDefinitionData> authorizationRoleDefinitionDataList = await GetAuthorizationRoleDefinitionDataListAsync(resourceId);
             AuthorizationRoleDefinitionData authorizationRoleDefinitionData = authorizationRoleDefinitionDataList.Where(authorizationRoleDefinitionData => authorizationRoleDefinitionData.RoleName.Equals(name)).First();
             Console.WriteLine($"Id: {authorizationRoleDefinitionData.Id} Name: {authorizationRoleDefinitionData.RoleName} Description: {authorizationRoleDefinitionData.Description} Type: {authorizationRoleDefinitionData.ResourceType}");
             return authorizationRoleDefinitionData;
         }
-        public async Task<RoleAssignmentData> GetRoleAssignmentCreateOrUpdateContentAsync(string resourceId, string name)
+        public async Task<RoleAssignmentData> CreateRoleAssignmentAsync(string resourceId, string name)
         {
-            Console.WriteLine($"Creating Role Assignment Create Or Update Content Async");
+            Console.WriteLine($"Creating Role Assignment Async");
             DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions() { TenantId = _tenentId });
             ArmClient armClient = new ArmClient(defaultAzureCredential);         
             AuthorizationRoleDefinitionData authorizationRoleDefinitionData = await GetAuthorizationRoleDefinitionData(resourceId: resourceId, name: name);
@@ -59,11 +73,35 @@ namespace Derby.Authorization.ClassLibrary
             ArmOperation<RoleAssignmentResource> armOperationRoleAssignmentResource = roleAssignmentResource.Update(WaitUntil.Completed, roleAssignmentCreateOrUpdateContent);
             RoleAssignmentResource roleAssignmentResource1 = armOperationRoleAssignmentResource.Value;
             RoleAssignmentData roleAssignmentData = roleAssignmentResource1.Data;
-            Console.WriteLine($"[Id]: {roleAssignmentData.Id} [Scope]: {roleAssignmentData.Scope} [Display Name]: {roleAssignmentData.Name}");
+            Console.WriteLine($"Id: {roleAssignmentData.Id} Scope: {roleAssignmentData.Scope} Display Name: {roleAssignmentData.Name}");
             return roleAssignmentData;
         }
-
-       
-
+        public async Task<RoleAssignmentData> GetRoleAssignmentAsync(string resourceId, string name)
+        {
+            Console.WriteLine($"Get Role Assignment Async");
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions() { TenantId = _tenentId });
+            ArmClient armClient = new ArmClient(defaultAzureCredential);
+            AuthorizationRoleDefinitionData authorizationRoleDefinitionData = await GetAuthorizationRoleDefinitionData(resourceId: resourceId, name: name);
+            ResourceIdentifier resourceIdentifier = RoleAssignmentResource.CreateResourceIdentifier(resourceId, "7e378bdc-84ac-4202-bd90-90b9392e6bfe");
+            RoleAssignmentResource roleAssignmentResource = armClient.GetRoleAssignmentResource(resourceIdentifier);
+            RoleAssignmentResource roleAssignmentResource2 = await roleAssignmentResource.GetAsync();
+            RoleAssignmentData roleAssignmentData = roleAssignmentResource2.Data;
+            Console.WriteLine($"Id: {roleAssignmentData.Id} Scope: {roleAssignmentData.Scope} Display Name: {roleAssignmentData.Name}");
+            return roleAssignmentData;
+        }
+        public async Task<RoleAssignmentData> DeleteRoleAssignmentAsync(string resourceId, string name)
+        {
+            Console.WriteLine($"Deleting Role Assignment Async");
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions() { TenantId = _tenentId });
+            ArmClient armClient = new ArmClient(defaultAzureCredential);
+            AuthorizationRoleDefinitionData authorizationRoleDefinitionData = await GetAuthorizationRoleDefinitionData(resourceId: resourceId, name: name);
+            ResourceIdentifier resourceIdentifier = RoleAssignmentResource.CreateResourceIdentifier(resourceId, "7e378bdc-84ac-4202-bd90-90b9392e6bfe");
+            RoleAssignmentResource roleAssignmentResource = armClient.GetRoleAssignmentResource(resourceIdentifier);
+            ArmOperation<RoleAssignmentResource> armOperationRoleAssignmentResource = await roleAssignmentResource.DeleteAsync(WaitUntil.Completed);
+            RoleAssignmentResource roleAssignmentResource2 = armOperationRoleAssignmentResource.Value;
+            RoleAssignmentData roleAssignmentData = roleAssignmentResource2.Data;
+            Console.WriteLine($"Id: {roleAssignmentData.Id} Scope: {roleAssignmentData.Scope} Display Name: {roleAssignmentData.Name}");
+            return roleAssignmentData;
+        }
     }
 }
