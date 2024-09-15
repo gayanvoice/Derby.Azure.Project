77252c31249297c3670ef4fc402bbe94678a1b56
gayanvoice <gayan.kuruppu@live.com>
2024-09-15 14:19:57 +0100

    Add: Derby.DigitalTwins -> Model and Twin Managers

---
+++ Derby.DigitalTwins.ClassLibrary/ModelManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Derby.DigitalTwins.ClassLibrary
+{
+    public class ModelManager
+    {
+        public ModelManager()
+        {
+        }
+
+        public void UploadModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Derby.DigitalTwins.ClassLibrary/TwinManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Derby.DigitalTwins.ClassLibrary
+{
+    public class TwinManager
+    {
+        public TwinManager()
+        {
+        }
+
+        public void CreateTwin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetTwin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTwinAsTelemetryEvent()
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateTwinAsPropertyEvent()
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteTwin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
