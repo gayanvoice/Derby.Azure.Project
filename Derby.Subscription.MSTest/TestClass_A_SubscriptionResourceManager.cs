135fdd59f1ebeb8063d47cdb471cd47fc2cc5d29
gayanvoice <gayan.kuruppu@live.com>
2024-09-15 18:53:59 +0100

    Update: MSTest -> Renamed to identify Test Methods

--- Derby.Subscription.MSTest/TestClass_A_SubscriptionResourceManager.cs
+++ Derby.Subscription.MSTest/TestClass_A_SubscriptionResourceManager.cs
@@ -4,7 +4,7 @@ using Derby.Subscription.ClassLibrary;
 namespace Derby.Subscription.MSTest
 {
     [TestClass]
-    public class TestClass_1_SubscriptionResourceManager
+    public class TestClass_A_SubscriptionResourceManager
     {
         private SubscriptionResourceManager _subscriptionResourceManager;
         private string _resourceGroupName;
@@ -17,7 +17,7 @@ namespace Derby.Subscription.MSTest
         }
         [Ignore]
         [TestMethod]
-        [DataRow(DisplayName = "Test Method A - Create Resource Group Resource Async")]
+        [DataRow(DisplayName = "Derby.Subscription.MSTest.A.A - Create Resource Group Resource Async")]
         public async Task TestMethod_A_CreateResourceGroupResourceAsync()
         {
             ResourceGroupResource resourceGroupResource = await _subscriptionResourceManager.CreateResourceGroupResourceAsync(_resourceGroupName);
@@ -25,14 +25,14 @@ namespace Derby.Subscription.MSTest
             Assert.AreEqual(_resourceGroupName, resourceGroupResource.Data.Name);
         }
         [TestMethod]
-        [DataRow(DisplayName = "Test Method B - Check If Resource Group Collection Exists Async")]
+        [DataRow(DisplayName = "Derby.Subscription.MSTest.A.B - Check If Resource Group Collection Exists Async")]
         public async Task TestMethod_B_CheckIfResourceGroupCollectionExistsAsync()
         {
             bool resourceGroupCollectionExists = await _subscriptionResourceManager.CheckIfResourceGroupCollectionExistsAsync(_resourceGroupName);
             Assert.IsTrue(resourceGroupCollectionExists);
         }
         [TestMethod]
-        [DataRow(DisplayName = "Test Method C - Get Resource Group Resource Async")]
+        [DataRow(DisplayName = "Derby.Subscription.MSTest.A.C - Get Resource Group Resource Async")]
         public async Task TestMethod_C_GetResourceGroupResourceAsync()
         {
             ResourceGroupResource resourceGroupResource = await _subscriptionResourceManager.GetResourceGroupResourceAsync(_resourceGroupName);
@@ -40,7 +40,7 @@ namespace Derby.Subscription.MSTest
             Assert.AreEqual(_resourceGroupName, resourceGroupResource.Data.Name);
         }
         [TestMethod]
-        [DataRow(DisplayName = "Test Method D - Get Resource Group Collection Async")]
+        [DataRow(DisplayName = "Derby.Subscription.MSTest.A.D - Get Resource Group Collection Async")]
         public async Task TestMethod_D_GetResourceGroupCollectionAsync()
         {
             ResourceGroupCollection resourceGroupCollection = await _subscriptionResourceManager.GetResourceGroupCollectionAsync();
@@ -49,7 +49,7 @@ namespace Derby.Subscription.MSTest
         }
         [Ignore]
         [TestMethod]
-        [DataRow(DisplayName = "Test Method E - Delete Resource Group Collection Async")]
+        [DataRow(DisplayName = "Derby.Subscription.MSTest.A.E - Delete Resource Group Collection Async")]
         public async Task TestMethod_E_CreateResourceGroupResource()
         {
             bool resourceGroupCollectionExists = await _subscriptionResourceManager.DeleteResourceGroupResourceAsync(_resourceGroupName);
